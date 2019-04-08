180a6a37e1b09269879c364932c01759494c057b
Tim Dommett <timdom11@gmail.com>
2019-04-08 20:32:17 +0200

    initial push to hosting servie

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const reviews = require("./routes/api/reviews");
 
 const express = require("express");
 const app = express();
+const path = require("path");
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 
 const db = require("./config/keys").mongoURI;
 mongoose
---
+++ config/key_prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Tim:00885522@cluster0-0ctlu.mongodb.net/test?retryWrites=true",
+  secretOrKey: "OKDiCKO2S6",
+  awsAccessKeyID: "AKIAJERVER5YUVM3ODTQ",
+  awsSecretAccessKey: "41Do4aXgYjKE4laIcqlYCZHDzODnOzcVyrK1HpQI",
+  googleMapsKey: "AIzaSyCzkuWHaGupBK9R7C5q25iN1C1l4ELn5pg"
+
+  //Make sure this is your own unique string
+};
