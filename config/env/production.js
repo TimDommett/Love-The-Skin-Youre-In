a3d8c27bd19382ef9fbdd9985304b1e78e4c96b5
Tim Dommett <timdom11@gmail.com>
2019-04-12 17:02:48 +0200

    initial push to hosting servie

---
+++ config/env/production.js
@@ -0,0 +1,5 @@
+server / config / env / production.js
+
+heroku config: add BUILDPACK_URL = https://github.com/mbuchetics/heroku-buildpack-nodejs-grunt.git
+
+heroku config: add NODE_ENV = production
