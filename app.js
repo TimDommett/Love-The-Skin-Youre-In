595008abf3ac1b8d63e20b366494bbe2b0c5c1f7
Tim Dommett <timdom11@gmail.com>
2019-04-15 17:32:39 +0200

    fixes for for heroku

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'));
-//     app.get('/', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 
 const db = require("./frontend/src/config/keys").mongoURI;
