a6746655d3f4548aeff9910483db251da096722b
Tim Dommett <timdom11@gmail.com>
2019-04-15 18:55:32 +0200

    Update keys.js

--- frontend/src/keys.js
+++ frontend/src/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-    awsAccessKeyID: "AKIAJERVER5YUVM3ODTQ",
-    awsSecretAccessKey: "41Do4aXgYjKE4laIcqlYCZHDzODnOzcVyrK1HpQI",
-  googleMapsKey: "AIzaSyCzkuWHaGupBK9R7C5q25iN1C1l4ELn5pg"
-  
-  //Make sure this is your own unique string
+  mongoURI: process.env.mongoURI,
+  secretOrKey: process.env.secretOrKey,
+  awsAccessKeyID: process.env.awsAccessKeyID,
+  awsSecretAccessKey: process.env.awsSecretAccessKey,
+  googleMapsKey: process.env.googleMapsKey
 };
