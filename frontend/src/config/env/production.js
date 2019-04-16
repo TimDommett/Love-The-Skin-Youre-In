dbb81bf1ebcaf9400f43b251b71c7d810c4d6a0a
Tim Dommett <timdom11@gmail.com>
2019-04-16 12:44:24 +0200

    code cleaning

--- frontend/src/config/env/production.js
+++
@@ -1,5 +0,0 @@
-server / config / env / production.js
-
-heroku config: add BUILDPACK_URL = https://github.com/mbuchetics/heroku-buildpack-nodejs-grunt.git
-
-heroku config: add NODE_ENV = production
